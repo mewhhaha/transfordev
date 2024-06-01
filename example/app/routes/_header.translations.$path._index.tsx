a32422c64fd333e5d7d4913255642ea4b97f4587
mewhhaha <kopatheonlyone@hotmail.com>
2024-06-02 01:01:46 +0200

    remove unused code

--- example/app/routes/_header.translations.$path._index.tsx
+++ example/app/routes/_header.translations.$path._index.tsx
@@ -4,8 +4,8 @@ import type {
   LoaderFunctionArgs,
 } from "@remix-run/cloudflare";
 import { defer } from "@remix-run/cloudflare";
-import { Await, Form, useLoaderData } from "@remix-run/react";
-import { Suspense, useEffect, useRef, useState } from "react";
+import { Await, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 import type { Routes } from "transfordev";
 
 export const loader = async ({ params, context }: LoaderFunctionArgs) => {
@@ -27,7 +27,7 @@ export const action = async ({ params, context }: ActionFunctionArgs) => {
 
 export default function Route() {
   const { path, deferred } = useLoaderData<typeof loader>();
-  const [edit, setEdit] = useState<Omit<EditProps, "onClose"> | null>(null);
+
   return (
     <main className="p-4">
       <h2 className="font-mono text-xl">{path}</h2>
@@ -44,18 +44,11 @@ export default function Route() {
 
               return sorted.map(({ language, value, date }) => {
                 return (
-                  <li key={language}>
-                    <button
-                      onClick={() => {
-                        setEdit({ value, language, date: new Date(date) });
-                      }}
-                      className="flex items-center"
-                    >
-                      <h3 className="w-12 flex-none text-left text-sm">
-                        {language}
-                      </h3>
-                      <p className="font-medium">{value || "---"}</p>
-                    </button>
+                  <li key={language} className="flex items-center">
+                    <h3 className="w-12 flex-none text-left text-sm">
+                      {language}
+                    </h3>
+                    <p className="font-medium">{value || "---"}</p>
                   </li>
                 );
               });
@@ -63,52 +56,6 @@ export default function Route() {
           </Await>
         </Suspense>
       </ol>
-      {edit && <Edit {...edit} onClose={() => setEdit(null)} />}
     </main>
   );
 }
-
-type EditProps = {
-  date: Date;
-  value: string;
-  language: string;
-  onClose: () => void;
-};
-
-const Edit = ({ date, value, language, onClose }: EditProps) => {
-  const ref = useRef<HTMLDialogElement>(null);
-
-  useEffect(() => {
-    ref.current?.showModal();
-  }, []);
-
-  return (
-    <dialog onClose={onClose} ref={ref} key={language}>
-      <Form method="POST">
-        <p className="text-xl">{language}</p>
-        <textarea className="h-32 w-full" defaultValue={value}>
-          {value}
-        </textarea>
-        <dl className="flex flex-col gap-1">
-          <div>
-            <dt className="text-sm font-medium">Updated at</dt>
-            <dd>
-              <time dateTime={date.toISOString()}>
-                {new Date(date).toLocaleDateString("en-us")}
-              </time>
-            </dd>
-          </div>
-        </dl>
-
-        <div className="mt-4 flex w-full justify-end">
-          <button className="rounded border-2 px-2 py-1" onClick={onClose}>
-            Close
-          </button>
-          <button className="rounded border-2 bg-black px-2 py-1 text-white">
-            Save
-          </button>
-        </div>
-      </Form>
-    </dialog>
-  );
-};
